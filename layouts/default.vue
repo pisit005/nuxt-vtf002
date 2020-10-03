22fc507456099575b3f8defd7ceb7193f7c300e3
pisit005 <68525342+pisit005@users.noreply.github.com>
2020-10-03 21:21:26 +0700

    first

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<style>
+  .theme--dark.v-application {
+    background-image:url('https://images8.alphacoders.com/999/thumb-1920-999804.jpg');
+    background-size: cover;
+}
+  h1{
+        font-family: 'Amatic SC', cursive;
+        color: #000000;
+}
+  h2{
+        font-family: 'Amatic SC', cursive;
+        color:#212121;
+}
+  .mor{
+        font-family: 'Sansita Swashed', cursive;
+        color:#ffffff;
+        font-size: 6rem;
+        text-align: center;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,49 @@
+<template lang="html">
+  <div>
+    <div class="mor">
+      Infinity
+    </div>
+    <div>
+      <v-text-field v-model="DayID" type="text" label="Day - DD(07)" color="#B71C1C" />
+      <v-text-field v-model="MonthID" type="text" label="Month - MM(03)" color="#B71C1C" />
+      <v-text-field v-model="YearID" type="text" label="Year - YYYY(2000)" color="#B71C1C" />
+      <v-btn color="#AB47BC" @click="kuku">
+        Search
+      </v-btn>
+      </v-date-picker>
+    </div>
+    <v-divider class="mt-2 mb-2" />
+    <div v-for="list in ID" :key="list.date">
+      <!-- {{list}} -->
+      <nuxt-link :to=" {name: 'product-id', params: { id: list }} ">
+        <img
+          style="max-width:100%"
+          :src="list.hdurl"
+        >
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      ID: null,
+      DayID: '',
+      MonthID: '',
+      YearID: '',
+      DateID: ''
+    }
+  },
+  methods: {
+    kuku () {
+      const url = 'https://api.nasa.gov/planetary/apod?date=' + this.YearID + '-' + this.MonthID + '-' + this.DayID + '&hd=true&api_key=iK6QWVlZ9w0RUsSNWob3UL5KcXFehRlXBHvKuW6p'
+      axios.get(url).then((response) => {
+        this.ID = response
+      })
+    }
+  }
+}
+</script>
+<style lang="css" scoped></style>
